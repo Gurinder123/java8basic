2fe9fd34028ea51e935b1b02377e765940c967ea
Gurinder singh <gurindersingh@xebia.com>
2016-08-17 15:05:02 +0530

    changing directory structure

--- java8basics/src/java8basics/DefenderMethod.java
+++ java8basics/src/java8basics/DefenderMethod.java
@@ -1,15 +1,15 @@
 package java8basics;
+
 interface Eatable {
-	default void eat() {
-		System.out.println("Yess i eat");
-	}
+    default void eat() {
+        System.out.println("Yess i eat");
+    }
 
 }
 
 public class DefenderMethod implements Eatable {
-	public static void main(String[] args) {
-
-		Eatable eat_obj = new DefenderMethod();
-		eat_obj.eat();
-	}
+    public static void main(String[] args) {
+        Eatable eat_obj = new DefenderMethod();
+        eat_obj.eat();
+    }
 }
